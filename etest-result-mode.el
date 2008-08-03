67b8b6adaa0133435953128353720fbc88a5aca6
Phil Jackson <phil@shellarchive.co.uk>
2008-08-03 19:13:00 +0100

    etest-result-mode.el: Added some docs.

--- etest-result-mode.el
+++ etest-result-mode.el
@@ -1,3 +1,30 @@
+;;; etest-result-mode.el --- Watch tests pass or fail
+
+;; Copyright (C) 2008 Philip Jackson
+
+;; Author: Philip Jackson <phil@shellarchive.co.uk>
+
+;; This file is not currently part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; The default result mode for etest.
+
 (require 'outline)
 
 (declare-function etest-resultp "etest")
